a45846ca364981325bbaa6997303c7c442fad2ca
Igor Popovic <igorp@better.care>
2025-05-10 20:37:45 +0200

    Initialize Spring Boot Kafka demo project

---
+++ src/main/java/com/igor/kafka/KafkaDemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.igor.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoSpringBootApplication.class, args);
+	}
+
+}
